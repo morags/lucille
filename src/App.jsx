2d1ed5c8a0df6878ffdcc9797b58ab1408c0e51f
anugya2 <as1255@student.london.ac.uk>
2022-08-11 22:10:28 -0400

    Navigation Bar
    Navigation bar completed. Haven't fixed console errors, sentence-case standards, and one eslint error yet.

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,74 @@
 import React, { useState } from 'react';
-import { Box, Center, ChakraProvider, Container, Tabs, TabList, TabPanels, Tab, TabPanel, Textarea, SimpleGrid } from '@chakra-ui/react';
-import { CalendarIcon, EditIcon } from '@chakra-ui/icons';
+import {
+  Box,
+  Center,
+  ChakraProvider,
+  Container,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Textarea,
+  SimpleGrid,
+} from '@chakra-ui/react';
 import './App.css';
 
 function Boxes(props) {
   return props.items.map((i) => <Box bg="lightgray" height="80px"><Center>{i}</Center></Box>);
 }
 
+function TabComp(props) {
+  const prop = props;
+  return (<Tab padding="3px" bg="#ededeb" margin="10px" boxShadow="5px 5px 5px rgb(137, 137, 137)" border="3px" borderColor="#a9a9a9" borderStyle="solid" borderRadius="10px"><img alt="fjsdkj" src={prop.fileLoc} /></Tab>);
+}
+
 function App() {
-  const [items, setItems] = useState([1, 2, 3, 4, 5, 6]);
+  // TODO: What does this do? Delete disableError.
+  const [items] = useState([1, 2, 3, 4, 5, 6]);
+  const navIconsLoc = ['/img/board_nav_btn.png', '/img/archive_nav_btn.png', '/img/guide_nav_btn.png', '/img/contact_nav_btn.png', '/img/setup_nav_btn.png'];
+  const navIcons = navIconsLoc.map((navIcon) => <TabComp fileLoc={navIcon} />);
 
   return (
     <ChakraProvider className="App">
-      <Container>
+      <Center w="100vw">
+        <Container w="720px" h="850px" bg="#e8e8e6" marginTop="20px" centerContent>
 
-        <Tabs variant="enclosed">
+          <Tabs align="center" size="lg" variant="enclosed">
 
-          <TabList>
-            <Tab><CalendarIcon /></Tab>
-            <Tab><EditIcon /></Tab>
-          </TabList>
+            <TabList>
+              {navIcons}
+            </TabList>
 
-          <TabPanels>
+            <TabPanels>
 
-            <TabPanel border="1px" borderColor="rgba(0, 0, 0, 0.08)">
-              <SimpleGrid columns={2} spacing={10}>
-                <Boxes items={items} />
-              </SimpleGrid>
-            </TabPanel>
+              <TabPanel border="3px" borderColor="#a9a9a9" borderStyle="solid" borderRadius="10px" bg="#cedcbf" h="450px">
+                <SimpleGrid columns={3} spacing={10}>
+                  <Boxes items={items} />
+                </SimpleGrid>
+              </TabPanel>
 
-            <TabPanel border="1px" borderColor="rgba(0, 0, 0, 0.08)">
-              <Textarea placeholder="Text" />
-            </TabPanel>
+              <TabPanel border="1px" borderColor="rgba(0, 0, 0, 0.08)">
+                <Textarea placeholder="Text" />
+              </TabPanel>
 
-          </TabPanels>
-        </Tabs>
+              <TabPanel border="1px" borderColor="rgba(0, 0, 0, 0.08)">
+                <Textarea placeholder="Text" />
+              </TabPanel>
 
-      </Container>
+              <TabPanel border="1px" borderColor="rgba(0, 0, 0, 0.08)">
+                <Textarea placeholder="Text" />
+              </TabPanel>
+
+              <TabPanel border="1px" borderColor="rgba(0, 0, 0, 0.08)">
+                <Textarea placeholder="Text" />
+              </TabPanel>
+
+            </TabPanels>
+          </Tabs>
+
+        </Container>
+      </Center>
 
     </ChakraProvider>
   );
---
+++ src/backup.jsx
@@ -0,0 +1,94 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import {
+  Box,
+  Center,
+  ChakraProvider,
+  Container,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Textarea,
+  SimpleGrid,
+  Flex,
+  Spacer,
+  Button,
+  ButtonGroup,
+  Stack,
+} from '@chakra-ui/react';
+import { CalendarIcon, EditIcon } from '@chakra-ui/icons';
+import './App.css';
+
+function Boxes(props) {
+  return props.items.map((i) => <Box bg="lightgray" height="80px"><Center>{i}</Center></Box>);
+}
+
+function NavBtn(props) {
+  return <a className="NavBtn" aria-label="Page" href={props.link}><img alt="icon" src={props.fileLoc} /></a>;
+}
+
+function FooterBtn(props) {
+  return <a className="footerBtn" aria-label="Page" href={props.link}><img alt="icon" src={props.fileLoc} /></a>;
+}
+
+function Navigation() {
+  return (
+    <navBar>
+      <NavBtn fileLoc="/img/board_nav_btn.png" link="/board" />
+      <NavBtn fileLoc="/img/archive_nav_btn.png" link="/board" />
+      <NavBtn fileLoc="/img/guide_nav_btn.png" link="/board" />
+      <NavBtn fileLoc="/img/contact_nav_btn.png" link="/board" />
+      <NavBtn fileLoc="/img/setup_nav_btn.png" link="/board" />
+    </navBar>
+  );
+}
+
+function Parchment(props) {
+  return (
+    <parchContainer>
+      <a className="ParchBtn" aria-label="Page" href={props.link}><img alt="icon" src="/img/parchment.png" /></a>
+      <textarea className="listTitle" type="text" value={props.title} />
+    </parchContainer>
+  );
+}
+
+function BoardPage(props) {
+  return (
+    <kanbanBoard>
+      <Parchment title="Weekend Shop" link="/board" />
+      <Parchment title="Gardening list" link="/board" />
+      <Parchment title="Gardening list" link="/board" />
+    </kanbanBoard>
+  );
+}
+
+function Footer(props) {
+  return (
+    <footerBar>
+      <FooterBtn fileLoc="/img/list_footer_btn.png" link="/board" />
+    </footerBar>
+  );
+}
+
+function App() {
+  const [items, setItems] = useState([1, 2, 3, 4, 5, 6]);
+  // pages = [board, archive, guide, contacts, setup]
+  return (
+    <body>
+      <Navigation />
+      {/* if pageChosen = boardPage then */}
+      <BoardPage />
+      <Footer />
+    </body>
+  );
+}
+
+export default App;
+
+
+<Tab color="#ededeb" boxShadow="dark-lg" border="3px" borderColor="#a9a9a9" borderStyle="solid" borderRadius="15px"><img alt="fjsdkj" src="/img/board_nav_btn.png" /></Tab>
