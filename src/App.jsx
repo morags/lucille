624bd7f09006f4a91477f6957eb4c75fbcb2ef5d
RodiLoo <33754316+Rodiloo@users.noreply.github.com>
2022-08-21 18:47:52 +0200

    change home path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import {
   Center,
   ChakraProvider,
@@ -49,7 +49,7 @@ function App() {
               <TabPanels>
                 <Routes>
                   <Route
-                    path="/lucille"
+                    path="/"
                     element={<Board />}
                     exact
                   />
