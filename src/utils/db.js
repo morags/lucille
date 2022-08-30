c4e9c96363e88912e934242bf16472dc37f7da56
RodiLoo <33754316+Rodiloo@users.noreply.github.com>
2022-08-31 00:52:53 +0200

    make the settings persistent

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,10 +1,35 @@
-import { Dexie } from 'dexie';
+import { Dexie } from "dexie";
 
 Dexie.debug = false;
 // eslint-disable-next-line import/prefer-default-export
-export const db = new Dexie('lucilleDB');
+export const db = new Dexie("lucilleDB");
 db.version(1).stores({
-  boards: '++id, name, archived, deleted, taskscount',
-  tasks: '++id, boardid, task, completed',
-  helpers: '++id, name, email, profilepicture'
+  boards: "++id, name, archived, deleted, taskscount",
+  tasks: "++id, boardid, task, completed",
+  helpers: "++id, name, email, profilepicture",
+  settings: "++id, fontsize, brightness, volume, vibration",
 });
+
+db.open();
+
+async function addDefaultSettings() {
+  try {
+    await db.settings.add({
+      fontsize: 30,
+      brightness: 100,
+      volume: 20,
+      vibration: 1,
+    });
+  } catch (error) { // eslint-disable-next-line
+    console.log(`Failed to add default settings ${error}`);
+  }
+}
+
+db.settings
+  .toArray()
+  .then((setting) => {
+    if (setting.length === 0) {
+        addDefaultSettings();
+    }
+  })
+  .catch((error) => error);
