c3ee0a554081ceae34c367c29466945f6af07b13
RodiLoo <33754316+Rodiloo@users.noreply.github.com>
2022-09-04 18:11:14 +0200

    dynamic guides fontsize and contrast

--- src/pages/Guide/Guide.jsx
+++ src/pages/Guide/Guide.jsx
@@ -25,36 +25,49 @@ function Guide({ smFont, fontBright, pathName }) {
       style={{ overflow: "auto" }}
       height="full"
     >
-      {guideData?.map((guide, i) => ( // Go through all the items in the guideData file via the map method to render them on the screen via an Accordian component
-        <Accordion key={guide.id} defaultIndex={[guide.id]} allowMultiple>
-          <AccordionItem
-            mb="20px"
-            border="2px"
-            rounded="xl"
-            boxShadow="md"
-            borderColor="gray.200"
-            backgroundColor="white"
-          >
-            <h2>
-              <AccordionButton>
-                <Box flex="1" textAlign="left">
-                  <Heading
-                    as="h3"
-                    style={{
-                      fontSize: `${smFont}px`,
-                      filter: `contrast(${fontBright}%)`,
-                    }}
-                  >
-                    {i + 1}. {guide.title}
-                  </Heading>
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel pb={4}>{guide.guide}</AccordionPanel>
-          </AccordionItem>
-        </Accordion>
-      ))}
+      {guideData?.map(
+        (
+          guide,
+          i // Go through all the items in the guideData file via the map method to render them on the screen via an Accordian component
+        ) => (
+          <Accordion key={guide.id} defaultIndex={[guide.id]} allowMultiple>
+            <AccordionItem
+              mb="20px"
+              border="2px"
+              rounded="xl"
+              boxShadow="md"
+              borderColor="gray.200"
+              backgroundColor="white"
+            >
+              <h2>
+                <AccordionButton>
+                  <Box flex="1" textAlign="left">
+                    <Heading
+                      as="h3"
+                      style={{
+                        fontSize: `${smFont}px`,
+                        filter: `contrast(${fontBright}%)`,
+                      }}
+                    >
+                      {i + 1}. {guide.title}
+                    </Heading>
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel
+                pb={4}
+                style={{
+                  fontSize: `${smFont}px`,
+                  filter: `contrast(${fontBright}%)`,
+                }}
+              >
+                {guide.guide}
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
+        )
+      )}
     </Box>
   );
 }
